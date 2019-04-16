a670aeee2a9373c3307d387deb063591de181b42
Алексей Гончаров <gon4arovlex@yandex.ru>
2019-04-16 08:49:25 +0300

    временно

--- src/app/core/auth/auth.component.ts
+++ src/app/core/auth/auth.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.scss']
+  styleUrls: ['./auth.component.scss'],
 })
-export class AuthComponent implements OnInit {
+export class AuthComponent {
+
+  public panelState: 'left-panel-active' | 'right-panel-active' = 'left-panel-active';
 
   constructor() { }
 
-  ngOnInit() {
+  public togglePanelState(): void {
+    if (this.panelState === 'left-panel-active') {
+      this.panelState = 'right-panel-active';
+    } else {
+      this.panelState = 'left-panel-active';
+    }
   }
-
 }
